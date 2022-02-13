654afe626cc41a34743216188e8e4d4b7674e98a
hernan-d <hernandrewes@gmail.com>
2022-02-12 22:30:44 -0300

    Putting the first lines of code

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = window.prompt("What's your choice?");
   // your code here!
 }
 
-function computerPlay() {
-    const selectionOptions = ['rock', 'paper', 'scissors'];
-    const handChosen = Math.floor(Math.random() * selectionOptions.length);
-    console.log(selectionOptions[handChosen]);
+function userPlay() {
+  let userChoice = window.prompt("What's your choice?");
+  let userChoiceFormatted = userChoice.toLowerCase();
+
+  if (
+    userChoiceFormatted == "rock" ||
+    userChoiceFormatted == "paper" ||
+    userChoiceFormatted == "scissors"
+  ) {
+    window.alert("Excelente");
+  } else {
+    window.alert("Please insert a valid play move");
+    let userChoice = window.prompt("What's your choice?");
+    let userChoiceFormatted = userChoice.toLowerCase();
+  }
+  console.log(userChoiceFormatted);
 }
 
-const playerSelection = "rock";
+function computerPlay() {
+  const selectionOptions = ["rock", "paper", "scissors"];
+  const handChosen = Math.floor(Math.random() * selectionOptions.length);
+  console.log(selectionOptions[handChosen]);
+}
+
+const playerSelection = userPlay();
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection);
+//console.log(playRound(playerSelection, computerSelection));
+//console.log(playerSelection);
